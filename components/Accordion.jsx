333ba9a67213dd65f5d196f0457a16d94345912f
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2024-03-09 14:11:02 -0800

    change it to match home page styling

--- components/Accordion.jsx
+++ components/Accordion.jsx
@@ -1,31 +1,28 @@
-import { ChevronDown, ChevronRight, Pencil } from "lucide-react";
+import {ChevronDown, ChevronRight, CircleEllipsis, Pencil} from "lucide-react";
 import Task from "@/components/Task";
-import { useState } from "react";
-import { useRouter } from "next/navigation"; // Use useRouter from "next/router" instead of "next/navigation"
+import {useState} from "react";
+import {useRouter} from "next/navigation"; // Use useRouter from "next/router" instead of "next/navigation"
 
-const Accordion = ({ title, content }) => {
+const Accordion = ({title, content}) => {
     const [isActive, setIsActive] = useState(false);
     const router = useRouter();
 
+
     return (
-        <div className="max-w-screen-md mx-auto">
+        <div className="flex flex-col w-full">
             {/* Clicking on this div toggles the accordion */}
             <div
                 onClick={() => setIsActive(!isActive)}
-                className="flex justify-between cursor-pointer p-2 border-b"
+                className="flex justify-between cursor-pointer p-1 items-center"
             >
-                <div className="flex items-center">
-                    {/* Conditional rendering of the chevron icon */}
-                    {isActive ? <ChevronDown size={10} /> : <ChevronRight size={10} />}
-                    <div className="ml-2 flex flex-row justify-center">
-                        {/* Display the title of the accordion */}
-                        {title}
-                        {/* Display the number of tasks */}
-                        <span className="bg-gray-200 px-1 py-0.5 rounded ml-2 text-center text-xs flex flex-row justify-center">
+                <span className="flex items-center gap-2 my-4">
+                    {isActive ? <ChevronDown size={20} color="#858585"/> : <ChevronRight size={20} color="#858585"/>}
+                    <p className="text-[#858585]">{title}</p>
+                    <span
+                        className="bg-gray-200 px-1 py-0.5 rounded ml-2 text-center text-xs flex flex-row justify-center">
                             {content.length}
                         </span>
-                    </div>
-                </div>
+                </span>
                 <div>
                     {/* Button to navigate to "/edit-course" */}
                     <button
@@ -36,15 +33,15 @@ const Accordion = ({ title, content }) => {
                         }}
                         className="px-3 py-1 text-sm bg-black rounded-full text-white"
                     >
-                        <Pencil size={13} color="#ffffff" />
+                        <Pencil size={13} color="#ffffff"/>
                     </button>
                 </div>
             </div>
             {/* Render tasks if the accordion is active */}
             {isActive && (
                 <div className="max-h-60 overflow-y-auto">
-                    {content.map(({ id, title, dueDate, type, status, course }) => (
-                        <Task key={id} name={title} type={type} dueDate={dueDate} state={status} />
+                    {content.map(({id, title, dueDate, type, status, course}) => (
+                        <Task key={id} name={title} type={type} dueDate={dueDate} state={status}/>
                     ))}
                 </div>
             )}
