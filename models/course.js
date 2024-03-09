bbe0b0d353ec123f6c29a42fb38a82a396b2deed
noufilsaqib <noufil.s@hotmail.com>
2024-03-08 21:33:07 -0800

    Create schema for course

---
+++ models/course.js
@@ -0,0 +1,35 @@
+import { Schema, model, models } from 'mongoose';
+
+const CourseSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required!'],
+    },
+    description: String,
+    credit: {
+        type: Number,
+        required: [true, 'Credit is required!'],
+    },
+    professor: String,
+    state: {
+        type: String,
+        enum: ['not_started', 'in_progress', 'completed'],
+        default: 'in_progress',
+    },
+    breakdown: [
+        {
+            type: {
+                type: String,
+                required: [true, 'Type is required!'],
+            },
+            percentage: {
+                type: Number,
+                required: [true, 'Percentage is required!'],
+            },
+        },
+    ],
+});
+
+const Course = models.Course || model('Course', CourseSchema);
+
+export default Course;
