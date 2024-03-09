6f7957b1c54d52123990259e276f9ca8c74558d3
Mangat Singh Toor | ਮੰਗਤ ਸਿੰਘ ਤੂਰ <immangat@gmail.com>
2024-03-09 12:35:49 -0800

    add edit course button

--- components/Accordion.jsx
+++ components/Accordion.jsx
@@ -2,56 +2,47 @@
 import {ChevronDown, ChevronRight, LucidePlus, Pencil} from "lucide-react";
 import Task from "@/components/Task";
 import {useState} from "react";
+import {useRouter} from "next/navigation";
 
 const Accordion = ({title, content}) => {
+
     const [isActive, setIsActive] = useState(false);
+    const router = useRouter();
 
-    return (<div style={{maxWidth: '800px', minWidth: '800px'}}>
-        <div
-            onClick={() => setIsActive(!isActive)}
-            style={{
-                display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-                cursor: 'pointer',
-                padding: '5px',
-                borderBottom: '1px solid #ccc',
-                alignContent: "center"
-            }}
-        >
 
-            <div style={{color: 'grey', display: "flex", justifyContent: "center", alignItems: "center"}}>
-
-                <div>
-                    {isActive ? <ChevronDown size={10}/> : <ChevronRight size={10}/>}
-                </div>
-                <div>
-                    {title}
-                </div>
-            </div>
+    return (
+        <div className="max-w-screen-md mx-auto">
             <div
-                style={{
-                    display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center"
-                }}
+                onClick={() => setIsActive(!isActive)}
+                className="flex justify-between cursor-pointer p-2 border-b"
             >
+                <div className="flex items-center">
+                    {isActive ? <ChevronDown size={10}/> : <ChevronRight size={10}/>}
+                    <div className="ml-2">{title}</div>
+                </div>
                 <div>
                     <button
-                        type='button'
-                        onClick={() => {}}
-                        className='px-3 py-1 text-sm bg-black rounded-full text-white'
-                    >
-                        <LucidePlus size={16} color="#ffffff"/>
-                    </button>
+                        type="button"
+                        onClick={(e) => {
+                            e.stopPropagation()
+                            router.push('/edit-course')
 
+                        }}
+                        className="px-3 py-1 text-sm bg-black rounded-full text-white"
+                    >
+                        <Pencil size={13} color="#ffffff"/>
+                    </button>
                 </div>
             </div>
-
+            {isActive && (
+                <div className="max-h-60 overflow-y-auto">
+                    {content.map(({id, title, dueDate, type, status, course}) => (
+                        <Task key={id} name={title} type={type} dueDate={dueDate} state={status}/>
+                    ))}
+                </div>
+            )}
         </div>
-        {isActive && (<div className="accordion-content" style={{maxHeight: '200px', overflowY: 'auto'}}>
-            {content.map((task, index) => (<Task key={index} name={task.name} type={task.type} dueDate={task.dueDate}
-                                                 state={task.state}/>))}
-        </div>)}
-    </div>);
+    );
 };
 
-export default Accordion
+export default Accordion;
